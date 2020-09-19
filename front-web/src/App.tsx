d2a81a61d0eb269f01e0bd00b4af8ec7e162da60
Allan Souza Silva <allan-souza16@hotmail.com>
2020-09-18 22:47:50 -0300

    Criando pagina home

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 
 import './App.css';
 import Header from './components/Header';
+import Home from './pages/Home';
 
 
 function App() {
   return (
     <div>
       <Header/>
+      <Home/>
+      
     </div>
   );
 }
